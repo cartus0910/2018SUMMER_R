9a7a0bd5008414d9b371ac040eb77222d1853bcf
cartus0910 <zoni0910@gmail.com>
2018-07-11 17:34:04 +0800

    ji

---
+++ week2/0711afternoon.R
@@ -0,0 +1,91 @@
+library(xml2)
+library(tmcn)
+library(rvest)
+
+#Function
+pttTestFunction <- function(URL, filename)
+{
+  #URL   = "https://www.ptt.cc/bbs/NTUcourse/index.html"
+  html  = read_html(URL)
+  title = html_nodes(html, "a")
+  href  = html_attr(title, "href")
+  data = data.frame(title = toUTF8(html_text(title)),
+                    href = href)
+  data = data[-c(1:10),]
+  getContent <- function(x) {
+    url  = paste0("https://www.ptt.cc", x)
+    tag  = html_node(read_html(url), 'div#main-content.bbs-screen.bbs-content')
+    text = toUTF8(html_text(tag))
+  }
+  #getContent(data$href[1])
+  allText = sapply(data$href, getContent)
+  allText
+  #out <- file(filename, "w", encoding="BIG-5") 
+  write.table(allText, filename) 
+  #close(out) 
+}
+
+id = c(1:5)
+URL = paste0("https://www.ptt.cc/bbs/Japan_Travel/index", id, ".html")
+filename = paste0(id, ".txt")
+pttTestFunction(URL[1], filename[1])
+mapply(pttTestFunction, 
+       URL = URL, filename = filename)
+
+rm(list=ls(all.names = TRUE))
+library(NLP)
+library(tm)
+library(jiebaRD)
+library(jiebaR)
+library(RColorBrewer)
+library(wordcloud)
+filenames <- list.files(getwd(), pattern="*.txt")
+files <- lapply(filenames, readLines)
+docs <- Corpus(VectorSource(files))
+#移除可能有問題的符號
+toSpace <- content_transformer(function(x, pattern) {
+  return (gsub(pattern, " ", x))
+}
+)
+docs <- tm_map(docs, toSpace, "※")
+docs <- tm_map(docs, toSpace, "◆")
+docs <- tm_map(docs, toSpace, "‧")
+docs <- tm_map(docs, toSpace, "的")
+docs <- tm_map(docs, toSpace, "我")
+docs <- tm_map(docs, toSpace, "是")
+docs <- tm_map(docs, toSpace, "看板")
+docs <- tm_map(docs, toSpace, "作者")
+docs <- tm_map(docs, toSpace, "發信站")
+docs <- tm_map(docs, toSpace, "批踢踢實業坊")
+docs <- tm_map(docs, toSpace, "[a-zA-Z]")
+docs <- tm_map(docs, toSpace, "推")
+docs <- tm_map(docs, toSpace, "有")
+docs <- tm_map(docs, toSpace, "就")
+docs <- tm_map(docs, toSpace, "了")
+docs <- tm_map(docs, toSpace, "不")
+docs <- tm_map(docs, toSpace, "在")
+docs <- tm_map(docs, toSpace, "都")
+docs <- tm_map(docs, toSpace, "也")
+docs <- tm_map(docs, toSpace, "很")
+docs <- tm_map(docs, toSpace, "們")
+docs <- tm_map(docs, toSpace, "到")
+#移除標點符號 (punctuation)
+#移除數字 (digits)、空白 (white space)
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+
+mixseg = worker()
+jieba_tokenizer=function(d){
+  unlist(segment(d[[1]],mixseg))
+}
+seg = lapply(docs, jieba_tokenizer)
+freqFrame = as.data.frame(table(unlist(seg)))
+freqFrame = freqFrame[order(freqFrame$Freq,decreasing=TRUE), ]
+library(knitr)
+kable(head(freqFrame), format = "markdown")
+new_freqFrame = subset(freqFrame, length(Var1)>=4)
+length('你')
+length('日本')
+
+wordcloud2(new_freqFrame, minSize=10, gridSize=5)
