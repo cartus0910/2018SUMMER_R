1504d894da20452cb70b5a4e83b1ca8ed35ace19
cartus0910 <zoni0910@gmail.com>
2018-08-05 23:16:40 +0800

    0805

--- final_pre/part1.R
+++ final_pre/part1.R
@@ -1,30 +1,56 @@
 setwd("D:/2018.07 R_DataAnalysis/2018SUMMER_R/data")
 
+#import data
 library(sf)
 library(ggplot2)
-worldmap <- st_read("countries.shp",stringsAsFactors=FALSE,quiet=TRUE)
-names(worldmap)[1] <- paste('Country.Name')
+worldmap <- st_read("TM_WORLD_BORDERS-0.3.shp",stringsAsFactors=FALSE,quiet=TRUE)
+names(worldmap)[3] <- paste('Country.Code')
 worldmap <- st_transform(worldmap, "+init=esri:54030")
-
 raw_gender <- read.csv('Gender_wewant.csv')
 
-df.sepa <- split(raw_gender, with(raw_gender, Series.Name), drop = TRUE)
+gender_map <- merge(worldmap, raw_gender, by="Country.Code")
 
-gender.var <- "Labor force, female (% of total labor force)"
-
-varnum <- which(names(df.sepa)==gender.var)
-df.need <- data.frame(df.sepa[varnum])
-names(df.need)[2] <- paste('Country.Name')
-gender_map <- merge(worldmap, df.need, by="Country.Name")
 library(stringr)
-for (i in 5:ncol(gender_map)-1){
+for (i in 15:ncol(gender_map)-1){
   names(gender_map)[i] <- str_sub(names(gender_map)[i],-5,-2)
+  if(i < 41){
+    gender_map[i+1] <- (gender_map[[i+1]] + gender_map[[i]]) /2
+  }
 }
 
-year = '2016'
+#plotting
+gender_map%>%
+  filter(Series.Name == 'Employers, female (% of female employment) (modeled ILO estimate)')%>%
+  select('2010')%>%
+  ggplot() +
+  geom_sf(aes(fill=`2010`))+
+  scale_fill_gradient(low = "#FFE4F3", high = "#F62018")
 
-gender_map[year] <- suppressWarnings(as.numeric(
-  levels(as.factor(gender_map[[year]]))[gender_map[[year]]]))
 
-ggplot() +
-  geom_sf(data=gender_map, aes(fill=gender_map[[year]]))
+
+ath <- read.csv("athlete_wewant.csv")
+ath['num']=1
+
+ath1<-ath%>%filter(Year>=1990)%>%group_by(NOC,Year,Sex)%>%summarize(Medal_sum=sum(Medal_score),people=sum(num))
+ath1A<-ath%>%filter(Year>=1990)%>%group_by(NOC,Year)%>%summarize(join_people=sum(num))
+ath1B<-unite(ath1A,NOC,NOC,Year,sep='_')
+
+ath2A<-ath1%>%filter(Sex=="F")
+ath2B<-unite(ath2A,NOC,NOC,Year,sep='_')
+ath2C<-data.frame(ath2B[,1],ath2B[,3])
+
+ath3<-merge(ath2C,ath1B,by="NOC")
+ath3["Fscore"]=ath3[,2]/ath3[,3]
+
+
+
+
+
+
+
+
+
+
+
+
+
